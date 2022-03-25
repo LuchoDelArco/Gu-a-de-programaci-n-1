0bb6877716c32a15ec01b57cfac459b8dec1b7d4
LuchoDelArco <47138072@est.ort.edu.ar>
2022-03-25 11:34:11 -0300

    Ex02 Completed. Changed scene name

---
+++ Assets/Ej02.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Ej02 : MonoBehaviour
+{
+	public int num1 = 4;
+	public int num2 = 5;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Debug.Log((num1 + num2));
+		Debug.Log(num1 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
